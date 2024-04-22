32d77ab3ac7ab40959720f8382369541e91c95a5
NaiWunJang <91878423+NaiWunJang@users.noreply.github.com>
2024-04-22 18:02:00 +0000

    Demo of a Flask Web-based elliptic curve encryption application.

--- main.py
+++ main.py
@@ -1,89 +1,26 @@
+from flask import Flask, render_template, request
 import hashlib
-import json
-from time import time
-from uuid import uuid4
-from flask import Flask, jsonify, request
-
-class Blockchain:
-    def __init__(self):
-        self.chain = []
-        self.current_transactions = []
-
-        # Create the genesis block
-        self.new_block(previous_hash='1', proof=100)
-
-    def new_block(self, proof, previous_hash=None):
-        """
-        Create a new Block in the Blockchain
-        :param proof: The proof given by the Proof of Work algorithm
-        :param previous_hash: Hash of previous Block
-        :return: New Block
-        """
-        block = {
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'transactions': self.current_transactions,
-            'proof': proof,
-            'previous_hash': previous_hash or self.hash(self.chain[-1]),
-        }
-
-        # Reset the current list of transactions
-        self.current_transactions = []
-
-        self.chain.append(block)
-        return block
-
-    def new_transaction(self, sender, recipient, amount):
-        """
-        Creates a new transaction to go into the next mined Block
-        :param sender: Address of the Sender
-        :param recipient: Address of the Recipient
-        :param amount: Amount
-        :return: The index of the Block that will hold this transaction
-        """
-        self.current_transactions.append({
-            'sender': sender,
-            'recipient': recipient,
-            'amount': amount,
-        })
-
-        return self.last_block['index'] + 1
-
-    @property
-    def last_block(self):
-        return self.chain[-1]
-
-    @staticmethod
-    def hash(block):
-        """
-        Creates a SHA-256 hash of a Block
-        :param block: Block
-        :return: SHA-256 hash
-        """
-        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
-        block_string = json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_string).hexdigest()
+import ecdsa
 
 app = Flask(__name__)
-blockchain = Blockchain()
 
-@app.route('/mine', methods=['GET'])
-def mine():
-    # We will mine a new block here
-    return "We will mine a new block"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/transactions/new', methods=['POST'])
-def new_transaction():
-    # We will add a new transaction here
-    return "We will add a new transaction"
+@app.route('/encrypt', methods=['POST'])
+def encrypt():
+    message = request.form.get('message')
+    private_key = ecdsa.SigningKey.generate()  # 生成私鑰
+    public_key = private_key.get_verifying_key()  # 從私鑰獲取公鑰
 
-@app.route('/chain', methods=['GET'])
-def full_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
-    }
-    return jsonify(response), 200
+    # 將消息進行 SHA-256 哈希
+    hashed_message = hashlib.sha256(message.encode()).hexdigest()
+
+    # 使用私鑰對哈希值進行簽名
+    signature = private_key.sign(hashed_message.encode())
+
+    return render_template('result.html', message=message, public_key=public_key, signature=signature)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
