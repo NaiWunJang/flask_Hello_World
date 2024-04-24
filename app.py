ce4afbc118f8a0e717d4f529b8c75625e732de04
NaiWunJang <91878423+NaiWunJang@users.noreply.github.com>
2024-04-24 12:48:30 +0000

    Restart

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+  from flask import Flask, render_template
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+  app = Flask(__name__)
+
+  @app.route('/')
+  def index():
+      return render_template('index.html')
+
+  if __name__ == '__main__':
+      app.run(debug=True)
