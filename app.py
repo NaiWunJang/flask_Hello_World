6182de7cd019694819e20c92a2762aca80f9338a
NaiWunJang <91878423+NaiWunJang@users.noreply.github.com>
2024-04-24 12:20:10 +0000

    Delete hash.py

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-from flask import Flask, request, render_template
-import hashlib
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def hash_text():
-    md5_hash = None
-    sha256_hash = None
-
-    if request.method == 'POST':
-        text = request.form['text']
-        md5_hash = hashlib.md5(text.encode()).hexdigest()
-        sha256_hash = hashlib.sha256(text.encode()).hexdigest()
-
-    return render_template('index.html', md5_hash=md5_hash, sha256_hash=sha256_hash)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
